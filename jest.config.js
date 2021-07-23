cd8ef9a5130964b2ca199bb2c87099938ae8c25c
rfprod <zoidenmacht@zoho.com>
2021-07-23 18:51:58 +0300

    chore(deps): bump project dependencies
    - [X] bump project dependencies to latest verions;
    - [X] apply migrations after updating project
    dependencies;
    - [X] fix coverage-stats.ts - cast explicit type on ject projects array because of
    new helper;
    - [X] use yarn audit:fix to fix issues automatically;
    - [X] fix rtc chat component
    incorrect remote description when accepting offer;
    
    BREAKING CHANGE: reinstall project dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,35 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/api',
-    '<rootDir>/apps/client',
-    '<rootDir>/apps/client-api',
-    '<rootDir>/apps/documentation',
-    '<rootDir>/apps/elements',
-    '<rootDir>/apps/elements-testing',
-    '<rootDir>/libs/backend-auth',
-    '<rootDir>/libs/backend-diagnostics',
-    '<rootDir>/libs/backend-grpc',
-    '<rootDir>/libs/backend-gql',
-    '<rootDir>/libs/backend-interfaces',
-    '<rootDir>/libs/backend-logger',
-    '<rootDir>/libs/backend-websocket',
-    '<rootDir>/libs/client-chatbot',
-    '<rootDir>/libs/client-components',
-    '<rootDir>/libs/client-core',
-    '<rootDir>/libs/client-d3-charts',
-    '<rootDir>/libs/client-diagnostics',
-    '<rootDir>/libs/client-directives',
-    '<rootDir>/libs/client-gql',
-    '<rootDir>/libs/client-material',
-    '<rootDir>/libs/client-services',
-    '<rootDir>/libs/client-sidebar',
-    '<rootDir>/libs/client-store',
-    '<rootDir>/libs/client-translate',
-    '<rootDir>/libs/client-util',
-    '<rootDir>/libs/client-unit-testing',
-    '<rootDir>/libs/client-user',
-    '<rootDir>/libs/client-workspaces',
-    '<rootDir>/libs/proto',
-    '<rootDir>/libs/client-pipes',
-  ],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
